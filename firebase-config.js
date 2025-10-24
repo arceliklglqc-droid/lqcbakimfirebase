65fc184d7b7dd7edb57ef9a4aee85f246f69d483
arceliklglqc-droid <arceliklglqc@gmail.com>
2025-10-24 08:14:20 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ÖNEMLİ: Bu dosya projenizi paylaşıyorsanız (örn: GitHub)
+// .gitignore dosyanıza eklenmeli ve asla herkese açık olarak paylaşılmamalıdır.
+
+// Firebase konsolunda "Proje Ayarları" (Dişli ikonu) > "Genel" sekmesinde
+// "Uygulamalarım" bölümünün altındaki "SDK kurulumu ve yapılandırması"ndan kopyalayın.
+const firebaseConfig = {
+  apiKey: "BURAYA_KENDİ_API_KEY_YAPIŞTIRIN",
+  authDomain: "BURAYA_KENDİ_AUTH_DOMAIN_YAPIŞTIRIN",
+  projectId: "BURAYA_KENDİ_PROJECT_ID_YAPIŞTIRIN",
+  storageBucket: "BURAYA_KENDİ_STORAGE_BUCKET_YAPIŞTIRIN",
+  messagingSenderId: "BURAYA_KENDİ_SENDER_ID_YAPIŞTIRIN",
+  appId: "BURAYA_KENDİ_APP_ID_YAPIŞTIRIN"
+};
